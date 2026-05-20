c357a7cce43c6afed0c7eaa9b57058497ed3f407
elhemide-creator <elhemide@gmail.com>
2026-05-20 21:13:55 +0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+// Bu kod qapını tam açıq saxlayır və Node.js (Edge) xətalarından yayınır
 export function middleware(request: NextRequest) {
-  // Qapını tam açıq qoyuruq - 500 xətası verməsi fiziki olaraq imkansızdır
   return NextResponse.next()
 }
 
 export const config = {
+  // Səhifələri oxuyarkən static faylları iqnor edir
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
