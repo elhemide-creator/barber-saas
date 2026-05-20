575939c32f9a2466a60a6d75fc4b0cc2fba5ac9b
elhemide-creator <elhemide@gmail.com>
2026-05-20 21:21:20 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Turbopack xətasını susdurmaq üçün:
+  // Turbopack xətasını susdurur
   turbopack: {},
-  // Vercel serverlərinin RAM-ı dolub çökməsin deyə (WorkerError həlli):
+  
+  // Vercel serverlərinin RAM-ı dolmasın deyə yaddaş limitini aktiv edir
   experimental: {
     memoryBasedWorkersCount: true,
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.ignoreWarnings = [{ module: /node_modules\/node-fetch/ }];
-    }
-    return config;
-  },
+  
+  // Build zamanı lazımsız yoxlamaları ləğv edib yaddaşa qənaət edir
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
